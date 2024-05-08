d33d0f7cfb754b0984a49f9b7a310ba40494151f
emanuelefavero <emafaux89@gmail.com>
2024-05-08 14:28:23 +0200

    update features/pointers

--- features/pointers.c
+++ features/pointers.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int a = 2;
-    // POINTERS, DECLARED WITH *var
-    // POINT TO THE HEX MEMORY ADDRESS OF A VARIABLE
-    int *b = NULL;
-    b = &a;
+int main(void) {
+  int a = 2;
+  // POINTERS, DECLARED WITH *var
+  // POINT TO THE HEX MEMORY ADDRESS OF A VARIABLE
+  int *b = &a;
 
-    // * is known as the dereference operator and is necessary to declare a pointer, while & is necessary to reference n
+  // * is known as the dereference operator and is necessary to declare a
+  // pointer, while & is necessary to reference n
 
-    printf("%p \n", &a); // prints hex address of variable a
-    printf("%p \n", b);  // prints hex address of variable a thru pointer b
+  printf("%p \n", &a); // prints hex address of variable a
+  printf("%p \n", b);  // prints hex address of variable a thru pointer b
+  printf("%d \n", *b); // prints value of variable a thru pointer b
 
-    return 0;
+  return 0;
 }
 
 // 0x16d8176c8
 // 0x16d8176c8
+// 2
--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
+  int a = 2;
+  // POINTERS, DECLARED WITH *var
+  // POINT TO THE HEX MEMORY ADDRESS OF A VARIABLE
+  int *b = &a;
 
-  // * Constants
-  const float PI = 3.14;
+  // * is known as the dereference operator and is necessary to declare a
+  // pointer, while & is necessary to reference n
 
-  printf("%.2f\n", PI);
+  printf("%p \n", &a); // prints hex address of variable a
+  printf("%p \n", b);  // prints hex address of variable a thru pointer b
+  printf("%d \n", *b); // prints value of variable a thru pointer b
 
   return 0;
 }
