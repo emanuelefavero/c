732b8a0347ba759a403a113b21e3a0f253348643
emanuelefavero <emafaux89@gmail.com>
2024-05-08 17:27:59 +0200

    update features/for_loop with break and continue

--- main.c
+++ main.c
@@ -2,9 +2,32 @@
 
 int main() {
 
+  // * for loop
   for (int i = 0; i < 5; i++) {
     printf("%d \n", i);
   }
+  // 0 1 2 3 4
+
+  // * break
+  for (int i = 0; i < 5; i++) {
+    if (i == 3) {
+      break;
+    }
+
+    printf("%d \n", i);
+  }
+  // 0 1 2
+
+  // * continue
+  for (int i = 0; i < 4; i++) {
+    if (i == 2) {
+      printf("hey \n");
+      continue;
+    } else {
+      printf("%d\n", i);
+    }
+  }
+  // 0 1 hey 3
 
   return 0;
 }
