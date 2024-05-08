733980740f2960b373a8bf76a6a10278018ea8fd
emanuelefavero <emafaux89@gmail.com>
2024-05-08 23:56:27 +0200

    udpate features/strings

--- features/strings.c
+++ features/strings.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 
-int main()
-{
-    // string
-    char x[] = "ciao";
-    printf("%s \n", x);
+int main() {
+  // * string
+  char greet[] = "ciao";
+  printf("%s \n", greet);
 
-    // get string user input with fgets(VAR, LONG, stdin)
-    // remember to declare variable beforehand
-    char name[50];
-    printf("what's your name: ");
-    fgets(name, 50, stdin);
-    printf("hi, %s \n", name);
+  // * get string user input with fgets(VAR, LONG, stdin)
+  char name[50];
+  printf("what's your name: ");
+  fgets(name, 50, stdin);
+  printf("hi, %s \n", name);
 
-    // string output, print string variable to the srcreen with fputs(VAR, stdout)
-    char city[50];
-    printf("what's your favorite city: ");
-    fgets(city, 50, stdin);
-    printf("your favorite city is ");
-    fputs(city, stdout);
+  // * print string variable to the screen with fputs(VAR, stdout)
+  // char city[50];
+  // printf("what's your favorite city: ");
+  // fgets(city, 50, stdin);
+  // printf("your favorite city is ");
+  // fputs(city, stdout);
 
-    return 0;
+  // * string length
+  char str[] = "hello";
+  int len = 0;
+  while (str[len] != '\0') {
+    len++;
+  }
+  printf("string length is %d \n", len);
+
+  int string_length =
+      sizeof(str) / sizeof(str[0]) - 1; // ? -1 to exclude null character
+  printf("string length is %d \n", string_length);
+
+  // * modify string
+  char a[10] = "hello";
+  a[0] = 'j';
+  printf("%s \n", a); // jello
+
+  // * loop through string
+  char b[] = "hola";
+
+  // TIP: You can loop though a string by checking if the character is not null
+  // ? \0 is the null character
+  for (int i = 0; b[i] != '\0'; i++) {
+    printf("%c \n", b[i]);
+  }
+
+  // * another way to define a string
+  /*
+  We can define a string by defining an array of characters and adding a null
+  character at the end
+  */
+  char c[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+  printf("%s \n", c);
+
+  return 0;
 }
--- main.c
+++ main.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 
-// c string standard library
-#include <string.h>
-
 int main() {
-  // strlen()
-  char name[] = "john";
-  printf("string length is %d \n", strlen(name));
+  // * string
+  char greet[] = "ciao";
+  printf("%s \n", greet);
 
-  // strcat() - concatenate strings
-  char a[20] = "hello ";
-  char b[20] = "how are you";
-  strcat(a, b);
-  printf("%s \n", a); // hello how are you
-  printf("%s \n", b); // how are you, a is modified, b is not
+  // * get string user input with fgets(VAR, LONG, stdin)
+  char name[50];
+  printf("what's your name: ");
+  fgets(name, 50, stdin);
+  printf("hi, %s \n", name);
 
-  // strcpy() - copy string to another
-  char c[20] = "hey";
-  char d[20];
-  // dest, source
-  strcpy(d, c);
-  printf("%s \n", d); // hey
+  // * print string variable to the screen with fputs(VAR, stdout)
+  // char city[50];
+  // printf("what's your favorite city: ");
+  // fgets(city, 50, stdin);
+  // printf("your favorite city is ");
+  // fputs(city, stdout);
+
+  // * string length
+  char str[] = "hello";
+  int len = 0;
+  while (str[len] != '\0') {
+    len++;
+  }
+  printf("string length is %d \n", len);
+
+  int string_length =
+      sizeof(str) / sizeof(str[0]) - 1; // ? -1 to exclude null character
+  printf("string length is %d \n", string_length);
+
+  // * modify string
+  char a[10] = "hello";
+  a[0] = 'j';
+  printf("%s \n", a); // jello
+
+  // * loop through string
+  char b[] = "hola";
+
+  // TIP: You can loop though a string by checking if the character is not null
+  // ? \0 is the null character
+  for (int i = 0; b[i] != '\0'; i++) {
+    printf("%c \n", b[i]);
+  }
+
+  // * another way to define a string
+  /*
+  We can define a string by defining an array of characters and adding a null
+  character at the end
+  */
+  char c[] = {'h', 'e', 'l', 'l', 'o', '\0'};
+  printf("%s \n", c);
 
   return 0;
 }
