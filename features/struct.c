a7173f2c80b608083ae6384237e023b46d72a2fa
emanuelefavero <emafaux89@gmail.com>
2024-05-09 19:18:19 +0200

    update features/struct

--- features/struct.c
+++ features/struct.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-// struct
-// it allows to create custom related data structure
-// similar to Javascript Class inside arrays, JS CODE:
-// class Person {
-//     constructor(name, number) {
-//         this.name = name;
-//         this.number = number;
-//     }
-// }
-// let people = [
-//     new Person("Brian", "+1-222-495-2000"),
-//     new Person("David", "+1-252-468-9050")
-// ];
-// console.log(people[0].name);
+/*
+* Structures
+struct allows to create custom related data structure
+similar to Javascript objects.
+NOTE: C Structures cannot have methods
+*/
 
-typedef struct
-{
-    char *name;
-    char *number;
-} person;
+typedef struct {
+  char *name;
+  int age;
+} User;
 
-int main(void)
-{
+int main() {
+  User user;
+  user.name = "John";
+  user.age = 25;
+  printf("Name: %s, Age: %d \n", user.name, user.age); // Name: John, Age: 25
 
-    person people[2];
+  // Assign values directly
+  User user2 = {"Jane", 30};
+  printf("Name: %s, Age: %d \n", user2.name, user2.age); // Name: Jane, Age: 30
 
-    people[0].name = "Brian";
-    people[0].number = "+1-222-495-2000";
-    people[1].name = "David";
-    people[1].number = "+1-252-468-9050";
+  // Copying values
+  User user3 = user2;
+  printf("Name: %s, Age: %d \n", user3.name, user3.age); // Name: Jane, Age: 30
 
-    char person_to_find[10] = "David";
+  // Modify values
+  user3.name = "Jack";
+  printf("Name: %s, Age: %d \n", user3.name, user3.age); // Name: Jack, Age: 30
 
-    for (int i = 0; i < 2; i++)
-    {
-        if (strcmp(people[i].name, person_to_find) == 0)
-        {
-            printf("found %s's number: %s \n", people[i].name, people[i].number);
-            return 0;
-        }
-    }
-    printf("%s not found \n", person_to_find);
-    return 1;
+  return 0;
 }
+
+/*
+TIP: You can't assign a string directly to a char array in structs so there are
+3 ways to do it:
+1. Use a pointer to a string (we did this in the example above): `char *name;`
+2. Define the string when you define the struct: `char name[20];` + `User user =
+{"John", 25};`
+3. Use strcpy: `char name[20];` + `strcpy(user.name, "John");`
+*/
--- main.c
+++ main.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+* Structures
+struct allows to create custom related data structure
+similar to Javascript objects.
+NOTE: C Structures cannot have methods
+*/
+
+typedef struct {
+  char *name;
+  int age;
+} User;
+
 int main() {
-  // * Read file
-  FILE *file = fopen("file.txt", "r");
+  User user;
+  user.name = "John";
+  user.age = 25;
+  printf("Name: %s, Age: %d \n", user.name, user.age); // Name: John, Age: 25
 
-  // Check if the file exists
-  if (file != NULL) {
+  // Assign values directly
+  User user2 = {"Jane", 30};
+  printf("Name: %s, Age: %d \n", user2.name, user2.age); // Name: Jane, Age: 30
 
-    // Store the content of the file in a string
-    int size = 100;
-    char content[size];
-    fgets(content, size, file);
+  // Copying values
+  User user3 = user2;
+  printf("Name: %s, Age: %d \n", user3.name, user3.age); // Name: Jane, Age: 30
 
-    // Print all lines of the file
-    // TIP: feof is used to determine if the end of the file has been reached
-    while (!feof(file)) {
-      printf("%s", content);
-      fgets(content, size, file);
-    }
-  } else {
-    printf("File does not exist \n");
-  }
-
-  // Close the file
-  fclose(file);
+  // Modify values
+  user3.name = "Jack";
+  printf("Name: %s, Age: %d \n", user3.name, user3.age); // Name: Jack, Age: 30
 
   return 0;
 }
+
+/*
+TIP: You can't assign a string directly to a char array in structs so there are
+3 ways to do it:
+1. Use a pointer to a string (we did this in the example above): `char *name;`
+2. Define the string when you define the struct: `char name[20];` + `User user =
+{"John", 25};`
+3. Use strcpy: `char name[20];` + `strcpy(user.name, "John");`
+*/
 
 // TIP: Use `cpace main.c` to run the code with auto compile
